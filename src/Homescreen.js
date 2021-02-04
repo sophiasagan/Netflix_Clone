f5ab31f0646b66fc2e6204f53c480224d1a8a043
Sophia Jung <sophiabraddock82@gmail.com>
2021-02-04 16:58:38 -0500

    navbar

---
+++ src/Homescreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Homescreen.css'
+import Nav from './Nav'
+
+function Homescreen() {
+    return (
+        <div className='homescreen'>
+            <Nav />
+            {/* Banner */}
+            {/* Row */}
+        </div>
+    )
+}
+
+export default Homescreen
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZGHjmMaGZ2KCLUY1Z1o4aPfmVhUnVVFM",
+    authDomain: "netflix-clone-2418e.firebaseapp.com",
+    projectId: "netflix-clone-2418e",
+    storageBucket: "netflix-clone-2418e.appspot.com",
+    messagingSenderId: "356240372765",
+    appId: "1:356240372765:web:5208269c29694e7dbd42f4"
+  };
+  
