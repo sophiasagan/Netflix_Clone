939f2cacb5a329167e792fe2d85e7fe733172cf8
Sophia Jung <sophiabraddock82@gmail.com>
2021-02-04 20:36:06 -0500

    banner

--- src/Homescreen.js
+++ src/Homescreen.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import './Homescreen.css'
-import Nav from './Nav'
+import React from "react";
+import Banner from "./Banner";
+import "./Homescreen.css";
+import Nav from "./Nav";
+import requests from "./Requests";
 
 function Homescreen() {
-    return (
-        <div className='homescreen'>
-            <Nav />
-            {/* Banner */}
-            {/* Row */}
-        </div>
-    )
+  return (
+    <div className="homescreen">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
 }
 
-export default Homescreen
+export default Homescreen;
--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
+            handleShow(true);
+        } else {
+            handleShow(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.removeEventListener('scroll', transitionNavBar)
+    }, [])
   return (
-    <div className="nav nav__black">
+    <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__content">
         <img
           className="nav__logo"
