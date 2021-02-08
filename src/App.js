0ec271914415c52969ad11a92678877b88904d7b
Sophia Jung <sophiabraddock82@gmail.com>
2021-02-07 19:54:18 -0500

    profile page

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ function App() {
           })
         );
       } else {
-        dispatch(logout);
+        dispatch(logout());
       }
     });
     return unsubscribe;
-  }, []);
+  }, [dispatch]);
   return (
     <div className="app">
       <Router>
