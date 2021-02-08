2578834bc43e8e5dfde2c8c0a304fa0dc8533e3f
Sophia Jung <sophiabraddock82@gmail.com>
2021-02-08 11:58:58 -0500

    plans cont

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
 import { auth } from "../firebase";
 import Nav from "../Nav";
+import PlanScreen from "./PlanScreen";
 import "./ProfileScreen.css";
 
 function ProfileScreen() {
@@ -20,8 +21,9 @@ function ProfileScreen() {
           <div className="profileScreen__details">
             <h2>{user.email}</h2>
             <div className="profileScreen__plans">
-                
+
                 <h3>Plans</h3>
+                <PlanScreen />
               <button onClick={() => auth.signOut()} className="profileScreen__signOut">Sign Out</button>
             </div>
           </div>
