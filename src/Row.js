9957d5790d9bc150423d84057c89adcac36b2f44
Sophia Jung <sophiabraddock82@gmail.com>
2021-02-08 13:49:25 -0500

    styling movie page

--- src/Row.js
+++ src/Row.js
@@ -1,15 +1,28 @@
-import React, { useEffect, useState, forwardRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
-import FlipMove from "react-flip-move";
 import YouTube from "react-youtube";
-const movieTrailer = require("movie-trailer");
+import movieTrailer from "movie-trailer";
+import { useLocation } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import StarRoundedIcon from "@material-ui/icons/StarRounded";
+import Rating from "@material-ui/lab/Rating";
+import TextTruncate from "react-text-truncate";
+import numeral from "numeral";
+
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-const Row = forwardRef(({ title, fetchUrl, isLargeRow = false }, ref) => {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
+  const { pathname } = useLocation();
+  const [showTrailer, setShowTrailer] = useState(false);
+
+  const getReleaseYear = (date) => {
+    let year = new Date(date);
+    return year.getFullYear();
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -20,6 +33,14 @@ const Row = forwardRef(({ title, fetchUrl, isLargeRow = false }, ref) => {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "430",
+    width: "840",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   const handleClick = (movie) => {
     if (trailerUrl) {
       setTrailerUrl("");
@@ -29,44 +50,61 @@ const Row = forwardRef(({ title, fetchUrl, isLargeRow = false }, ref) => {
           const urlParams = new URLSearchParams(new URL(url).search);
           setTrailerUrl(urlParams.get("v"));
         })
-        .catch((e) => console.log(e));
+        .catch((error) => alert("Sorry, There is no trailer for this movie"));
     }
-  };
-
-  const opts = {
-    height: "390",
-    width: "100%",
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-    },
   };
 
   return (
     <div className="row">
       <h2>{title}</h2>
 
-      <FlipMove className="row__posters">
-        {movies.map(
-          (movie) =>
-            ((isLargeRow && movie.poster_path) ||
-              (!isLargeRow && movie.backdrop_path)) && (
-              <img
-                ref={ref}
-                onClick={() => handleClick(movie)}
-                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                key={movie.id}
-                src={`${base_url}${
-                  isLargeRow ? movie.poster_path : movie.backdrop_path
-                }`}
-                alt={movie.name}
+      <div className="row__posters list__items" >
+        {movies.map((movie) => (
+          <div class="list__item" onClick={() => handleClick(movie)}>
+            <img
+              loading="lazy"
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+            <div className="list__itemInfo">
+              <h5 className="list__itemTitle">
+                {movie.title || movie.original_title}
+                <span className="list__itemYear">
+                  ({getReleaseYear(movie.release_date || movie.first_air_date)})
+                </span>
+              </h5>
+              <TextTruncate
+                line={2}
+                element="p"
+                containerClassName="list__itemOverview"
+                truncateText="…"
+                text={movie.tagline || movie.overview}
               />
-            )
-        )}
-      </FlipMove>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+              <div className="list__rating">
+                <Rating
+                  name="movie-rating"
+                  className="movieRating"
+                  value={movie.vote_average / 2 || 0}
+                  precision={0.5}
+                  icon={<StarRoundedIcon fontSize="inherit" readOnly />}
+                />
+                <small className="list__likes">
+                  {numeral(movie.vote_average / 2).format("0.0")}
+                </small>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="trailer">
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
     </div>
   );
-});
+}
 
 export default Row;
